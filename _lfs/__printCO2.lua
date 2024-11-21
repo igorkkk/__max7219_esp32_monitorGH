ca1fc9dede562c10ca19eeeb078b743529f13ee9
Igor Kandaurov <igorkandaurov@gmail.com>
2024-11-21 12:12:23 +0300

    Fix

--- _lfs/__printCO2.lua
+++ _lfs/__printCO2.lua
@@ -1,5 +1,4 @@
 do
-<<<<<<< HEAD
     prt('\t\t\t\t\t\t\t!!!Work CO2:', wth.co2)
     dat.notime = true
     dofile 'dco2.lua'
@@ -9,19 +8,4 @@ do
             OUT('C' .. wth.co2)
         end
     )
-=======
-    prt('\t\t\t\t\t\t\t!!!Work CO2:' , wth.co2 )
-   -- if wth.co2 and wth.co2 > 1500 then
-        dat.notime = true
-        dofile'dco2.lua'
-        tmr.create():alarm(1500, tmr.ALARM_AUTO, 
-        function (t)
-                t:stop(); t:unregister(); t = nil
-                OUT('C' .. wth.co2)
-            end
-        )
-    --else
-      --  dofile'__printtime.lua'
-    -- end
->>>>>>> ddfac53bba2a3e7e9adafee4324f58c78d9d4837
 end
--- _lfs/__printPM2_5.lua
+++ _lfs/__printPM2_5.lua
@@ -1,7 +1,6 @@
 do
     local pm = 0
     prt('\n\n\t\t\t\t\t\t\t!!!work pm2_5', wth.pm2_5)
-<<<<<<< HEAD
     dat.notime = true
     dofile 'dpm2_5.lua'
     pm = tonumber(wth.pm2_5)
@@ -12,20 +11,4 @@ do
             OUT('pm' .. pm)
         end
     )
-=======
-    --if wth.pm2_5 and tonumber(wth.pm2_5) > 25 then
-        dat.notime = true
-        dofile'dpm2_5.lua'
-        pm = tonumber(wth.pm2_5)
-        if pm > 99 then pm = math.floor(pm) end
-        tmr.create():alarm(1500, tmr.ALARM_AUTO, 
-        function (t)
-                t:stop(); t:unregister(); t = nil
-                OUT('pm' .. pm)
-            end
-        )
-    --else
-    --    dofile'__printtime.lua'
-    --end
->>>>>>> ddfac53bba2a3e7e9adafee4324f58c78d9d4837
 end
