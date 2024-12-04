6ba9b5e030593b74a49459aa3d913ae5f80df0b1
Igor Kandaurov <igorkandaurov@gmail.com>
2024-12-04 13:48:32 +0300

    Weather Changes

--- _lfs/__printcodeFC.lua
+++ _lfs/__printcodeFC.lua
@@ -1,10 +1,12 @@
 do
     local filename = ''
     if wth.codeFCOM then
-        if wth.codeFCOM < 3 then
+        if wth.codeFCOM < 2 then
             filename = 'dclearn.lua'
-        elseif wth.codeFCOM == 3 then
+        elseif wth.codeFCOM == 2 then
             filename = 'dcloudn.lua'
+        elseif wth.codeFCOM == 3 then
+            filename = 'dhmuron.lua'
         elseif wth.codeFCOM == 45 then
             filename = 'd45.lua'
         elseif wth.codeFCOM == 48 then
@@ -15,12 +17,18 @@ do
             filename = 'ddrizzlesupern.lua'
         elseif wth.codeFCOM > 60 and wth.codeFCOM < 69 then
             filename = 'dreinn.lua'
-        elseif wth.codeFCOM > 70 and wth.codeFCOM < 78 then
+        elseif wth.codeFCOM == 71 then
             filename = 'dsnown.lua'
+        elseif wth.codeFCOM == 73 then
+            filename = 'dmeteln.lua'
+        elseif wth.codeFCOM == 75 then
+            filename = 'dviegan.lua'
+        elseif wth.codeFCOM == 77 then
+            filename = 'dpurgan.lua'
         elseif wth.codeFCOM > 79 and wth.codeFCOM < 83 then
             filename = 'dshowern.lua'
         elseif wth.codeFCOM == 85 or wth.codeFCOM == 86 then
-            filename = 'dsnown.lua'
+            filename = 'dburann.lua'
         elseif wth.codeFCOM > 94 and wth.codeFCOM < 100 then
             filename = 'dstormn.lua'
         end
